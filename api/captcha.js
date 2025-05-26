41bbe5769e0207746316ce405756a8799978444d
Aaron Smith <arnhsmith@gmail.com>
2025-05-26 16:33:24 -0400

    fixed captcha.js

--- api/captcha.js
+++ api/captcha.js
@@ -2,8 +2,6 @@ import { v4 as uuidv4 } from 'uuid';
 import svgCaptcha from 'svg-captcha';
 import { setCaptcha } from '../lib/captcha-store.js';
 
-const captchaStore = new Map();
-
 export default function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).end();
@@ -12,12 +10,11 @@ export default function handler(req, res) {
   const captcha = svgCaptcha.create();
   const id = uuidv4();
 
-  captchaStore.set(id, {
+  // Use the shared memory store so `captcha-image.js` can access it
+  setCaptcha(id, {
     answer: captcha.text.toLowerCase(),
     image: captcha.data,
   });
-
-  setTimeout(() => captchaStore.delete(id), 10 * 60 * 1000);
 
   res.status(200).json({
     captcha_id: id,
